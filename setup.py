6d91f5ca7f6f913374a9d6a92b9822f3b2731b42
Daniel McKnight <daniel@neongecko.com>
2023-01-26 14:10:11 -0800

    Add PyPI Automation

--- setup.py
+++ setup.py
@@ -46,20 +46,37 @@ def find_resource_files():
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("./version.py", "r", encoding="utf-8") as v:
-    for line in v.readlines():
-        if line.startswith("__version__"):
-            if '"' in line:
-                version = line.split('"')[1]
-            else:
-                version = line.split("'")[1]
+
+def get_version():
+    """ Find the version of this skill"""
+    version_file = path.join(path.dirname(__file__), 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
 
 
 setup(
     name=PYPI_NAME,
-    version=version,
+    version=get_version(),
     description='mycroft/ovos news skill plugin',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url=URL,
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
